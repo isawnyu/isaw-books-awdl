232b7079e2f11d085242c74c5c5f52a6f66b0faa
Sebastian Heath <sebastian.heath@gmail.com>
2016-10-20 16:15:11 -0400

    moving along

--- oasis-city/mk_xhtml.sh
+++
@@ -1,4 +0,0 @@
-cp /Users/sfsh/Documents/isaw/isaw-digital-books-private/oasis-city/oasis-city.xhtml .
-cp /Users/sfsh/Documents/isaw/isaw-digital-books-private/oasis-city/css/*.css css/
-
-xsltproc --nonet oc2xhtml.xsl oasis-city.xhtml
