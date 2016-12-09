aab64c5a80b81836bd0fdad955d3ff5b3f8ec1e8
Sebastian Heath <sebastian.heath@gmail.com>
2016-12-09 14:32:21 -0500

    catchup

--- oasis-city/resized-images/mk.sh
+++ oasis-city/resized-images/mk.sh
@@ -1,3 +1,3 @@
-mogrify -path . -resize 1024 /Users/sfsh/Documents/isaw/isaw-digital-books-private/oasis-city/images/*.jpg
-mogrify -path . -resize 1024 /Users/sfsh/Documents/isaw/isaw-digital-books-private/oasis-city/images/*.png
+mogrify -path . -resize 768 /Users/sfsh/Documents/isaw/isaw-digital-books-private/oasis-city/images/*.jpg
+mogrify -path . -resize 768 /Users/sfsh/Documents/isaw/isaw-digital-books-private/oasis-city/images/*.png
 
