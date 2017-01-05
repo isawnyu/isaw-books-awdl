a81a68038f6e9b4964231cf6802d87a71042ffe6
Sebastian Heath <sebastian.heath@gmail.com>
2017-01-05 08:46:40 -0500

    hatke

--- hatke2013-aksum-and-nubia-awdl/mk_html.sh
+++ hatke2013-aksum-and-nubia-awdl/mk_html.sh
@@ -1,3 +1,3 @@
 # cp ../../isaw/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.html index.xhtml
-cp ../../isaw/hatke2013-aksum-and-nubia/hatke_map*.png .
-xsltproc --nonet hatke2xhtml.xsl ../../isaw/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.xhtml > index.xhtml
+cp ../../isaw/isaw-digital-books-private/hatke2013-aksum-and-nubia/hatke_map*.png .
+xsltproc --nonet hatke2xhtml.xsl ../../isaw/isaw-digital-books-private/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.xhtml > index.xhtml
